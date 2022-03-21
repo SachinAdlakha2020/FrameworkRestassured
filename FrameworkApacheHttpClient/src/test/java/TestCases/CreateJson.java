9f1d8b51d8a5dbfe0c6c4908654e8a98c98ff904
SachinAdlakha2020 <102018609+SachinAdlakha2020@users.noreply.github.com>
2022-03-21 19:19:36 +0530

    Added comments to check the proper message

--- FrameworkApacheHttpClient/src/test/java/TestCases/CreateJson.java
+++ FrameworkApacheHttpClient/src/test/java/TestCases/CreateJson.java
@@ -10,6 +10,7 @@ public class CreateJson {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Added comments.
 		Gson gson = new Gson();
 		EmployeeInformation dat= new EmployeeInformation(1, "Sadlakha@Xeeva.com", "Sachin", "Adlakha", "Avatar");
 		SupportInformation support = new SupportInformation("xeeva.com", "Works on Spend Data");
