46cc37d2fa419960abe429d832b3cf69870369c8
SachinAdlakha2020 <102018609+SachinAdlakha2020@users.noreply.github.com>
2022-03-29 17:07:02 +0530

    Add date time stamp in fiel so that unique file get generated.

--- FrameworkRestassured/src/test/java/TestSuiteEmployee/PostEmployee.java
+++ FrameworkRestassured/src/test/java/TestSuiteEmployee/PostEmployee.java
@@ -6,7 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
@@ -164,54 +168,60 @@ public class PostEmployee extends BaseClass {
 
 	private void WriteOutput(List<EmployeeData> inputDataList, List<EmployeeData> outputDataList) {
 
-		String outputFilePath = "E:\\Automation\\TestData\\Output\\TestDataInExcel.xlsx";
+		//Creating the unique file name with current date and time.
+		SimpleDateFormat formatter = new SimpleDateFormat("YYYYMMdd-HHmmss");
+		Date date = new Date();
+		String currentDateTime = formatter.format(date);
+		String fileName = "EmployeeResults_" +  currentDateTime + ".xlsx";
+		String outputFilePath = "E:\\Automation\\TestData\\Output\\" + fileName;
 		// Blank workbook
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		// Create a blank sheet
 		XSSFSheet sheet = workbook.createSheet("Employee Data");
 
 		Row rows = null;
-		
+
 		Cell cells = null;
 		int rowsSize = inputDataList.size();
-		
-		  if(rowsSize>0) { rows = sheet.createRow(0); AddHeader(rows, cells, 0); }
-		 
+
+		if (rowsSize > 0) {
+			rows = sheet.createRow(0);
+			AddHeader(rows, cells, 0);
+		}
 
 		for (int row = 0; row < rowsSize; row++) {
-			
-			System.out.println("Rows Count: " + row);
-			rows = sheet.createRow(row+1);
 
-				
+			System.out.println("Rows Count: " + row);
+			rows = sheet.createRow(row + 1);
+
 			int cellCount = 0;
-				// Compare the outcome and store in excel
-				String result = "true";
-				
-				if (inputDataList.get(row).name.equals(outputDataList.get(row).name)) {
-					result = "Pass";
-					cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).name,
-							outputDataList.get(row).name, result);
-					System.out.println("Cells Count: " + cellCount);
-				} else {
-					result = "Fail";
-					cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).name,
-							outputDataList.get(row).name, result);
-					System.out.println("Cells Count: " + cellCount);
-				}
-				if (inputDataList.get(row).job.equals(outputDataList.get(row).job)) {
-					result = "Pass";
-					cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).job,
-							outputDataList.get(row).job, result);
-					System.out.println("Cells Count: " + cellCount);
-				} else {
-					result = "fail";
-					cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).job,
-							outputDataList.get(row).job, result);
-					System.out.println("Cells Count: " + cellCount);
-				}
+			// Compare the outcome and store in excel
+			String result = "true";
+
+			if (inputDataList.get(row).name.equals(outputDataList.get(row).name)) {
+				result = "Pass";
+				cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).name, outputDataList.get(row).name,
+						result);
+				System.out.println("Cells Count: " + cellCount);
+			} else {
+				result = "Fail";
+				cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).name, outputDataList.get(row).name,
+						result);
+				System.out.println("Cells Count: " + cellCount);
 			}
-		
+			if (inputDataList.get(row).job.equals(outputDataList.get(row).job)) {
+				result = "Pass";
+				cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).job, outputDataList.get(row).job,
+						result);
+				System.out.println("Cells Count: " + cellCount);
+			} else {
+				result = "fail";
+				cellCount = AddCells(rows, cells, cellCount, inputDataList.get(row).job, outputDataList.get(row).job,
+						result);
+				System.out.println("Cells Count: " + cellCount);
+			}
+		}
+
 		try {
 			// Write the workbook in file system
 			FileOutputStream out = new FileOutputStream(new File(outputFilePath));
@@ -223,7 +233,7 @@ public class PostEmployee extends BaseClass {
 		}
 	}
 
-	private void AddHeader(Row rows, Cell cells,int cellCount) {
+	private void AddHeader(Row rows, Cell cells, int cellCount) {
 		System.out.println("Adding Header: ");
 		cells = rows.createCell(cellCount);
 		cells.setCellValue("Actual Name");
@@ -237,7 +247,7 @@ public class PostEmployee extends BaseClass {
 		cells.setCellValue("Expected Job");
 		cells = rows.createCell(++cellCount);
 		cells.setCellValue("Result Job");
-		//++cellCount;
+		// ++cellCount;
 	}
 
 	private int AddCells(Row rows, Cell cells, int cellCount, String inputValue, String outputValue, String result) {
