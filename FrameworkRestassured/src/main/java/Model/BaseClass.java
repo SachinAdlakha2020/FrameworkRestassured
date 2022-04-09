9f996546818577fbae41561b618d4dbc5232890d
SachinAdlakha2020 <102018609+SachinAdlakha2020@users.noreply.github.com>
2022-04-09 13:56:04 +0530

    Modifiction are done to mark the methods as Pass/Fail

--- FrameworkRestassured/src/main/java/Model/BaseClass.java
+++ FrameworkRestassured/src/main/java/Model/BaseClass.java
@@ -3,15 +3,26 @@ package Model;
 import static io.restassured.RestAssured.*;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
 import io.restassured.RestAssured;
 
 public class BaseClass {
 
+	public static String recordResult;
+	public static String fieldResult;
+	
+	
 	@BeforeClass
 	public static void SetUp() {
 		RestAssured.baseURI ="https://reqres.in/";
-		RestAssured.basePath ="api/";
+		RestAssured.basePath ="api/";		
 		
+	}
+
+	@BeforeMethod
+	public void InitializeValues() {
+		recordResult="Pass";
+		fieldResult="Pass";
 	}
 }
